25b2faa18530448e853ccdb65895ab94e73c5197
mintRaven-05 <debjeetbanerjee48@gmail.com>
2024-12-20 20:06:42 +0530

    (modify spacing): slight changes

--- src/config.go
+++ src/config.go
@@ -18,13 +18,13 @@ type Config struct{
 	defaultActiveTab_light        string
 	defaultActiveTab_dark         string
 
-	Tab1_FocusActive 		     string
-	Tab1_FocusInactive  	     string
+	Tab1_FocusActive 		      string
+	Tab1_FocusInactive  	      string
 	Tab1_TableSelectedBackground  string
 	Tab1_TableSelectedForeground  string
 	Tab1_SpinnerColor             string
-	Tab1_SpinnerMsgColor 		 string
-	Tab1_kaizen_AscciArtColor 	 string
+	Tab1_SpinnerMsgColor 		  string
+	Tab1_kaizen_AscciArtColor 	  string
 }
 
 
