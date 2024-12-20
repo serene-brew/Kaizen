4a273fe18bac583c860875dbf9651ebbb9f8410d
mintRaven-05 <debjeetbanerjee48@gmail.com>
2024-12-20 19:54:39 +0530

    (modify): update config handler for LoadConfig() to parse config.yaml

--- src/config.go
+++ src/config.go
@@ -6,6 +6,9 @@ import (
 	"path/filepath"
 	"github.com/spf13/viper"
 )
+/* Config struct defines the color configuration for different elements of the application.
+ * It includes attributes for foreground, unfocused states, active tabs, and specific settings
+ * for Tab1 such as focus state, table selection, spinner, and ASCII art colors.*/
 
 type Config struct{
  	defaultForeground_light       string
@@ -25,3 +28,51 @@ type Config struct{
 }
 
 
+/* LoadConfig function initializes the Config struct by reading values from a YAML configuration file.
+ * It uses the Viper library to locate and parse the configuration file, which is expected
+ * to be found in the "~/.config/kaizen" directory under the name "config.yaml".
+ * The function returns a populated Config struct instance.*/
+
+func LoadConfig() Config{
+	configPath := expandPath("~/.config/kaizen")
+
+	viper.SetConfigFile(filepath.Join(configPath, "config.yaml"))
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	var conf Config
+
+	defaultForeground_dark := viper.GetString("DefaultForeground.dark")
+	defaultForeground_light := viper.GetString("DefaultForeground.light")
+	defaultUnfocused_light := viper.GetString("DefaultUnfocused.light")
+	defaultUnfocused_dark := viper.GetString("DefaultUnfocused.dark")
+	defaultActiveTab_light := viper.GetString("DefaultActiveTab.light")
+	defaultActiveTab_dark := viper.GetString("DefaultActiveTab.dark")
+
+ 	Tab1_FocusActive := viper.GetString("Tab1.focus.active")
+	Tab1_FocusInactive := viper.GetString("Tab1.focus.inactive")
+	Tab1_TableSelectedForeground := viper.GetString("Tab1.table.selected.foreground")
+	Tab1_TableSelectedBackground := viper.GetString("Tab1.table.selected.background")
+	Tab1_SpinnerColor := viper.GetString("Tab1.spinner.color")
+	Tab1_SpinnerMsgColor := viper.GetString("Tab1.spinner.msg.color")
+	Tab1_kaizen_AscciArtColor := viper.GetString("Tab1.ASCII Art.color")
+
+	conf.defaultUnfocused_dark = defaultUnfocused_dark
+	conf.defaultUnfocused_light = defaultUnfocused_light
+	conf.defaultForeground_light = defaultForeground_light
+	conf.defaultForeground_dark = defaultForeground_dark
+	conf.defaultActiveTab_light = defaultActiveTab_light
+	conf.defaultActiveTab_dark = defaultActiveTab_dark
+
+	conf.Tab1_FocusActive = Tab1_FocusActive
+	conf.Tab1_FocusInactive = Tab1_FocusInactive
+	conf.Tab1_TableSelectedForeground = Tab1_TableSelectedForeground
+	conf.Tab1_TableSelectedBackground = Tab1_TableSelectedBackground
+	conf.Tab1_SpinnerColor = Tab1_SpinnerColor
+	conf.Tab1_SpinnerMsgColor = Tab1_SpinnerMsgColor
+	conf.Tab1_kaizen_AscciArtColor = Tab1_kaizen_AscciArtColor
+
+	return conf
+}
