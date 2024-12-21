e20492fd5f01dae900786878186864912f124f3c
mintRaven-05 <debjeetbanerjee48@gmail.com>
2024-12-21 08:59:00 +0530

    create keymaps for better keybind management (i am tired of changing the keybinds in every single fucking file)

---
+++ src/keymap.go
@@ -0,0 +1,59 @@
+package main
+
+import "github.com/charmbracelet/bubbles/key"
+
+//pre-defined keymaps for the TUI
+type keyMap struct {
+	List1  key.Binding
+	List2 key.Binding
+	Table key.Binding
+	Enter key.Binding
+	Input key.Binding
+	Esc   key.Binding
+	Tab   key.Binding
+	CtrlTab key.Binding
+}
+
+/* newKeyMap 
+ * ---------
+ * returns a keyMap consisting of keybinds
+ * used a shared function but for the TUI
+*/
+func newKeyMap() keyMap {
+	return keyMap{
+		Input: key.NewBinding(
+			key.WithKeys("!"),
+			key.WithHelp("!", "focus input"),
+		),
+		List1: key.NewBinding(
+			key.WithKeys("#"),
+			key.WithHelp("#", "focus list one"),
+		),
+		List2: key.NewBinding(
+			key.WithKeys("$"),
+			key.WithHelp("$", "focus list two"),
+		),
+		Table: key.NewBinding(
+			key.WithKeys("@"),
+			key.WithHelp("@", "focus table"),
+		),
+		Enter: key.NewBinding(
+			key.WithKeys("enter"),
+			key.WithHelp("enter", "Action"),
+		),
+		Esc: key.NewBinding(
+			key.WithKeys("esc"),
+			key.WithHelp("esc", "quit"),
+		),
+		Tab: key.NewBinding(
+			key.WithKeys("tab"),
+			key.WithHelp("tab", "switch tabs forward"),
+		),
+		CtrlTab: key.NewBinding(
+			key.WithKeys("ctrl+tab"),
+			key.WithHelp("ctrl+tab", "switch tabs backward"),
+		),
+
+
+	}
+}
