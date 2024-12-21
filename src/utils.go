4039bdebe99eec70462bde8a4c2b23472b8beb3b
mintRaven-05 <debjeetbanerjee48@gmail.com>
2024-12-21 08:19:49 +0530

    (modify): remove expandPath() utility function from utility.go

--- src/utils.go
+++ src/utils.go
@@ -13,22 +13,6 @@ import (
 )
 
 /*
-expandPath is a utility function that expands a given path starting with `~` to the full user home directory path.
-If the path does not start with `~`, it is returned unchanged.
-It uses the os.UserHomeDir function to obtain the home directory.
-*/
-func expandPath(path string) string {
-	if len(path) > 0 && path[:1] == "~" {
-		homeDir, err := os.UserHomeDir()
-		if err != nil {
-			panic(err) // Handle error as needed
-		}
-		return filepath.Join(homeDir, path[1:])
-	}
-	return path
-}
-
-/*
 generateRows is a method of Tab1Model that converts a two-dimensional slice of interface{} data into a slice of table.Row.
 Each row in the table is constructed by extracting and formatting specific fields from the input data.
 The rows are intended to be displayed in a tabular Bubble Tea model.
