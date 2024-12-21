d80e157ecfa9fc5caa05610ee64ecb2b7638eded
mintRaven-05 <debjeetbanerjee48@gmail.com>
2024-12-21 17:37:15 +0530

    entrypoint for the app (main package)

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+import(
+	"flag"
+	kaizen "github.com/serene-brew/Kaizen/src"
+)
+
+//Main entrypoint for the application
+func main() {
+	uninstalFlag := flag.Bool("uninstall", false, "Run the uninstaller script")
+	updateFlag := flag.Bool("update", false, "Run the update script")
+	versionFlag := flag.Bool("v", false, "views version information") 
+	flag.Parse()
+
+	if *uninstalFlag {
+		kaizen.RunUninstalScript()
+	} else if *versionFlag{
+		kaizen.ViewVersion()
+	}else if *updateFlag{
+		kaizen.RunUpdateScript()
+	}else{
+		kaizen.ExecuteAppStub()
+	}
+}
