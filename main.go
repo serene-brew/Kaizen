5beda41fa6220edb22061423c4f75be230b5aea5
mintRaven-05 <debjeetbanerjee48@gmail.com>
2024-12-25 18:17:05 +0530

    (modify): invoke AutoHeal and check of external dependencies installed or not

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"os"
+	"fmt"
 	"flag"
 	kaizen "github.com/serene-brew/Kaizen/src"
 )
 
 // Main entrypoint for the application
 func main() {
+	//perform auto-heal check before starting kaizen
+	kaizen.AutoHeal()
+	
+	//check whether MPV-player is installed or not
+	_, err := os.Stat("/usr/bin/mpv")
+	if err != nil {
+		fmt.Println("[!] Please install MPV-player using your package manager before running kaizen")
+		os.Exit(1)
+	}
+
+	//kaizen CLI flags
 	uninstalFlag := flag.Bool("uninstall", false, "Run the uninstaller script")
 	updateFlag := flag.Bool("update", false, "Run the update script")
 	versionFlag := flag.Bool("v", false, "views version information")
