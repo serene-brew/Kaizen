36db5fe69219d2d59c72e78f172536596dd350d1
mintRaven <debjeetbanerjee48@gmail.com>
2025-05-13 17:04:25 +0530

    [BUG]: fix overall width problem with table columns

--- src/WatchAnime.go
+++ src/WatchAnime.go
@@ -113,8 +113,8 @@ func NewTab1Model() Tab1Model {
 	columns := []table.Column{
 		{Title: centerText("", 10), Width: 10},
 		{Title: "Anime Title", Width: 75},
-		{Title: centerText("Sub Episodes", 20), Width: 20},
-		{Title: centerText("Dub Episodes", 20), Width: 20},
+		{Title: centerText("Sub Episodes", 15), Width: 15},
+		{Title: centerText("Dub Episodes", 15), Width: 15},
 		{Title: centerText("Score", 15), Width: 15},
 		{Title: centerText("Rating", 15), Width: 15},
 		{Title: centerText("Status", 15), Width: 15},
